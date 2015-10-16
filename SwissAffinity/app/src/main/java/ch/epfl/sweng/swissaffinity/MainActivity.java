8f191bd2e66f1f74e54e9afd076fe8ec2ff00f43
Mehmet Furkan ŞAHİN <sahinffurkan@gmail.com>
2015-10-16 17:30:03 +0200

    Initial Preferences commit

--- SwissAffinity/app/src/main/java/ch/epfl/sweng/swissaffinity/MainActivity.java
+++ SwissAffinity/app/src/main/java/ch/epfl/sweng/swissaffinity/MainActivity.java
@@ -1,5 +1,6 @@
 package ch.epfl.sweng.swissaffinity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -15,6 +16,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent myIntent = new Intent(MainActivity.this, SettingsActivity.class);
+        MainActivity.this.startActivity(myIntent);
+
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
