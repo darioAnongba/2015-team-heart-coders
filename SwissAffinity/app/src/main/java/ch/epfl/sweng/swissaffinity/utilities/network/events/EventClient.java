87516085348402c119c629449ba23ac48aa0691d
Lionel Fleury <lionel.fleury@epfl.ch>
2015-11-15 23:20:03 +0100

    EventClient clean up

--- SwissAffinity/app/src/main/java/ch/epfl/sweng/swissaffinity/utilities/network/events/EventClient.java
+++ SwissAffinity/app/src/main/java/ch/epfl/sweng/swissaffinity/utilities/network/events/EventClient.java
@@ -8,12 +8,50 @@ import java.util.List;
 import ch.epfl.sweng.swissaffinity.events.Event;
 import ch.epfl.sweng.swissaffinity.utilities.Location;
 
+/**
+ * Representation of an event client.
+ */
 public interface EventClient {
+
+    /**
+     * Fetch all the events
+     *
+     * @return the list of all the events
+     *
+     * @throws EventClientException
+     */
     List<Event> fetchAll() throws EventClientException;
 
+    /**
+     * Fetch all the events for the given locations
+     *
+     * @param locations
+     *
+     * @return all the events according the locations
+     *
+     * @throws EventClientException
+     */
     List<Event> fetchAllFor(Collection<Location> locations) throws EventClientException;
 
+    /**
+     * Fetch a single event given its ID
+     *
+     * @param id the event ID
+     *
+     * @return the desired event
+     *
+     * @throws EventClientException
+     */
     Event fetchBy(int id) throws EventClientException;
 
+    /**
+     * Fetch the image for the given event
+     *
+     * @param event the event
+     *
+     * @return the image {@link Bitmap}
+     *
+     * @throws EventClientException
+     */
     Bitmap imageFor(Event event) throws EventClientException;
 }
