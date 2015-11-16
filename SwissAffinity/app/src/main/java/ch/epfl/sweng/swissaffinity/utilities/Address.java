f8bd295b4b0c0b33bbf95cbe10a3a89098742edc
Lionel Fleury <lionel.fleury@epfl.ch>
2015-11-16 16:37:38 +0100

    Address clean up

--- SwissAffinity/app/src/main/java/ch/epfl/sweng/swissaffinity/utilities/Address.java
+++ SwissAffinity/app/src/main/java/ch/epfl/sweng/swissaffinity/utilities/Address.java
@@ -3,34 +3,45 @@ package ch.epfl.sweng.swissaffinity.utilities;
 import java.io.Serializable;
 
 /**
- * Represents a Postal Address
- * Created by dario on 16.10.2015.
+ * Representation of a postal address.
  */
 public class Address implements Serializable {
-    private String street;
-    private int streetNumber;
-    private int zipCode;
-    private String city;
-    private String province;
-    private String country;
+    private final String mStreet;
+    private final int mStreetNumber;
+    private final int mZipCode;
+    private final String mCity;
+    private final String mProvince;
+    private final String mCountry;
 
     /**
      * Create a new postal address
      *
-     * @param country the country code  (fr, ch, en, de, ...)
-     * @param zipCode the zipCode
-     * @param city the city name
-     * @param province the area or province
+     * @param country      the country code  (fr, ch, en, de, ...)
+     * @param zipCode      the zipCode
+     * @param city         the city name
+     * @param province     the area or province
      * @param streetNumber the street number
-     * @param street the street name
+     * @param street       the street name
      */
-    public Address(String country, int zipCode, String city, String province, int streetNumber, String street) {
-        this.country = country;
-        this.zipCode = zipCode;
-        this.city = city;
-        this.province = province;
-        this.streetNumber = streetNumber;
-        this.street = street;
+    public Address(
+            String country,
+            int zipCode,
+            String city,
+            String province,
+            int streetNumber,
+            String street)
+    {
+        if (country == null || zipCode < 0 || city == null || province == null ||
+            streetNumber < 0 || street == null)
+        {
+            throw new IllegalArgumentException();
+        }
+        mCountry = country;
+        mZipCode = zipCode;
+        mCity = city;
+        mProvince = province;
+        mStreetNumber = streetNumber;
+        mStreet = street;
     }
 
     /**
@@ -39,16 +50,7 @@ public class Address implements Serializable {
      * @return street
      */
     public String getStreet() {
-        return street;
-    }
-
-    /**
-     * Set the street name
-     *
-     * @param street the street name
-     */
-    public void setStreet(String street) {
-        this.street = street;
+        return mStreet;
     }
 
     /**
@@ -57,16 +59,7 @@ public class Address implements Serializable {
      * @return zip code
      */
     public int getZipCode() {
-        return zipCode;
-    }
-
-    /**
-     * set zipCode
-     *
-     * @param zipCode the zip code
-     */
-    public void setZipCode(int zipCode) {
-        this.zipCode = zipCode;
+        return mZipCode;
     }
 
     /**
@@ -75,16 +68,7 @@ public class Address implements Serializable {
      * @return streetNumber
      */
     public int getStreetNumber() {
-        return streetNumber;
-    }
-
-    /**
-     * Set the street name
-     *
-     * @param streetNumber the street number
-     */
-    public void setStreetNumber(int streetNumber) {
-        this.streetNumber = streetNumber;
+        return mStreetNumber;
     }
 
     /**
@@ -93,16 +77,7 @@ public class Address implements Serializable {
      * @return city
      */
     public String getCity() {
-        return city;
-    }
-
-    /**
-     * Set city name
-     *
-     * @param city the city name
-     */
-    public void setCity(String city) {
-        this.city = city;
+        return mCity;
     }
 
     /**
@@ -111,16 +86,7 @@ public class Address implements Serializable {
      * @return province
      */
     public String getProvince() {
-        return province;
-    }
-
-    /**
-     * Set province
-     *
-     * @param province the area or province
-     */
-    public void setProvince(String province) {
-        this.province = province;
+        return mProvince;
     }
 
     /**
@@ -129,14 +95,6 @@ public class Address implements Serializable {
      * @return country
      */
     public String getCountry() {
-        return country;
-    }
-
-    /**
-     * Set country code
-     * @param country The country code (fr, de, ch, etc...)
-     */
-    public void setCountry(String country) {
-        this.country = country;
+        return mCountry;
     }
 }
